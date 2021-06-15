41867cfc9eb11833851a68aaf5ca43d0b6350b59
fevencat <fevencat@student.42sp.org.br>
2021-06-15 17:16:44 +0000

    Soluçao para os desafio da Rush00

---
+++ Rush/Rush00/ex00/main.c
@@ -0,0 +1,32 @@
+void	rush(int x, int y);
+void	ft_putchar(char c);
+
+void	run_test (void)
+{
+	ft_putchar('\n');
+	rush(5, 3);
+	ft_putchar('\n');
+	rush(5, 1);
+	ft_putchar('\n');
+	rush(1, 1);
+	ft_putchar('\n');
+	rush(1, 5);
+	ft_putchar('\n');
+	rush(4, 4);
+	ft_putchar('\n');
+}
+
+int	main(int argc, char **argv)
+{
+	/*
+	if (argc > 1)
+		rush(argv[1][0] - '0', argv[2][0] - '0');
+	else
+		run_test ();
+	*/
+	int i = 0;
+	if (argc > 1)
+	while ()
+		ft_putchar('%');
+	return (0);
+}
---
+++ Rush/Rush00/ex00/rush03.c
@@ -0,0 +1,44 @@
+#define TOP_LEFT_CHAR 'A'
+#define TOP_MID_CHAR 'B'
+#define TOP_RIGHT_CHAR 'C'
+#define MID_LEFT_CHAR 'B'
+#define MID_MID_CHAR ' '
+#define MID_RIGHT_CHAR 'B'
+#define BOT_LEFT_CHAR 'A'
+#define BOT_MID_CHAR 'B'
+#define BOT_RIGHT_CHAR 'C'
+void	ft_putchar (char c);
+
+void	print_line (int length, char first, char mid, char last)
+{
+	int	col_count;
+
+	ft_putchar (first);
+	if (length > 1)
+	{
+		col_count = 1;
+		while (col_count++ < length - 1)
+			ft_putchar (mid);
+		ft_putchar (last);
+	}
+	ft_putchar ('\n');
+}
+
+//y == height;x == width;
+void	rush (int x, int y)
+{
+	int	count_line;
+
+	if (x != 0 || y != 0)
+	{
+		print_line (x, TOP_LEFT_CHAR, TOP_MID_CHAR, TOP_RIGHT_CHAR);
+		if (y > 2)
+		{
+			count_line = 0;
+			while (count_line++ < y - 2)
+				print_line (x, MID_LEFT_CHAR, MID_MID_CHAR, MID_RIGHT_CHAR);
+		}
+		if (y > 1)
+			print_line (x, BOT_LEFT_CHAR, BOT_MID_CHAR, BOT_RIGHT_CHAR);
+	}
+}
