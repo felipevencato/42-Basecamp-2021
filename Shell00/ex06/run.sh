64c7b5f61321f6353e8b7d2bc93079c3d403df6b
fevencat <fevencat@student.42sp.org.br>
2021-06-15 17:15:05 +0000

    Adicionado outra forma de resolver o problema ex06

--- Shell00/ex06/run.sh
+++ Shell00/ex06/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 rm diff a sw.diff
 tar -xf resources.tar.gz
-cp a b
-patch b sw.diff
+# cp a b
+# patch b sw.diff
+# O comando abaixo faz o mesmo que os dois comando acima
+patch a -o b sw.diff
 cat b
 diff a b > diff
 diff diff sw.diff
